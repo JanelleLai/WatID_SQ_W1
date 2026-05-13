ecac7c3d6d5c13cb14b0b0fa47299ecb6c102ae6
Janelle Lai <j34lai@uwaterloo.ca>
2026-05-13 19:20:37 -0400

    Added image in assets folder and code in sketch, image, text, and shape

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
+function preload() {
+  img = loadImage("assest/image.jpg");
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 500);
 }
 
 function draw() {
   background(0, 0, 255);
+  image(img, 0, 0, width, height);
+  rect(50, 50, 10, 50);
+  text("Hello World", 50, 200);
 }
